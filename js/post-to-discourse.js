69bc8aa88446b619e4c569f9b8d2ecc0614ec5ae
scossar <scossar3@gmail.com>
2016-09-21 22:06:42 -0700

    Don't serialize the whole form

--- js/post-to-discourse.js
+++ js/post-to-discourse.js
@@ -2,10 +2,16 @@ jQuery(document).ready(function () {
 
     jQuery("#submit-post-to-discourse").click(function (event) {
 
-        // var postContent = jQuery( "#x" ).val(),
-        var url = post_to_discourse_script.ajaxurl,
-            data = jQuery('#post-to-discourse').serialize();
-            console.log(url);
+        var postContent = jQuery( "#x" ).val(),
+            topicID = jQuery('#topic_id').val(),
+            nonce = jQuery('#post_to_discourse_nonce').val(),
+            data = {
+                'action': 'create_discourse_post',
+                'nonce': nonce,
+                'post_content': postContent,
+                'topic_id': topicID
+            },
+            url = post_to_discourse_script.ajaxurl;
 
         jQuery.post(url, data, function(response) {
             console.log(response);
